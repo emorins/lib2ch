ef55d8898e6568dd3702f52d2cf45146660ef632
Shotaro Emori <emorins@gmail.com>
2011-01-05 06:55:04 +0900

    add dylib

--- sample/main.c
+++ sample/main.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
-#include <lib2ch.h>
+#include "../src/lib2ch.h"
 
 #define MAX_BUF 640000
 
 int main(int argc, char *args[])
 {
-  /*
-  if (argc != 4) {
-    printf("usage: ./main [server] [boerd] [thread]\n");
+  if (argc != 3) {
+    printf("usage: ./main [server] [boerd]\n");
     return -1;
   }
-  */
   
   Ch ch;
   lib2ch_init(&ch);
 
   Board board;
-  lib2ch_board_init(&board, &ch, "kamome", "hikky");
+  lib2ch_board_init(&board, &ch, args[1], args[2]);
 
   Thread thread;
   lib2ch_thread_init(&board);
@@ -35,7 +33,6 @@ int main(int argc, char *args[])
     }
     printf("%s:%s\n", response.name, response.subject);
   }
-  
 
   return 0;
 }
